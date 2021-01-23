44084ce6da7f2b68e7f2cb96973a884f87e9810c
Salman Masood (Al-Farsi) <salmanmasood24@yahoo.com>
2021-01-23 21:48:52 +0500

    filter.js added

---
+++ WebApplication1/WebApplication1/Scripts/Filters.js
@@ -0,0 +1,14 @@
+﻿/// <reference path="javascript.js" />
+
+app.filter("gender", function () {
+    return function (gender) {
+        switch (gender) {
+            case 1:
+                return "Male";
+            case 2:
+                return "Female";
+            case 3:
+                return "Not disclosed";
+        }
+    }
+});
--- WebApplication1/WebApplication1/Scripts/JavaScript.js
+++ WebApplication1/WebApplication1/Scripts/JavaScript.js
@@ -1,15 +1,17 @@
-﻿var myApp = angular.module("myModule", []);
+﻿/// <reference path="angular.js" />
+
+var app = angular.module("myModule", []);
 
 
 var data = { empid: 1, firstName: "salman", desgination: "Software-Engineer", image: "https://localhost:44394/Images/1.jpg", gender: "Male", salary: 155000, dateOfBirth: new Date("August 15, 1974"), likes: 10, dislikes: 0 };
 var employees = [
-    { firstName: "Ben", lastName: "Hastings", gender: "Male", salary: 55000, dateOfBirth: new Date("August 22, 1964"), likes: 10, dislikes: 0 },
-    { firstName: "Sara", lastName: "Paul", gender: "Female", salary: 68000, dateOfBirth: new Date("August 13, 1934") ,likes: 0, dislikes: 0},
-    { firstName: "Mark", lastName: "Holland", gender: "Male", salary: 57000, dateOfBirth: new Date("January 15, 1954"),likes: 0, dislikes: 0 },
-    { firstName: "Pam", lastName: "Macintosh", gender: "Female", salary: 53000, dateOfBirth: new Date("August 23, 1924"),likes: 0, dislikes: 0 },
-    { firstName: "Todd", lastName: "Barber", gender: "Male", salary: 60000, dateOfBirth: new Date("August 12, 1975"), likes: 0, dislikes: 0}, data
+    { firstName: "Ben", lastName: "Hastings", gender: 1, salary: 55000, dateOfBirth: new Date("August 22, 1964"), likes: 10, dislikes: 0 },
+    { firstName: "Sara", lastName: "Paul", gender: 2, salary: 68000, dateOfBirth: new Date("August 13, 1934") ,likes: 0, dislikes: 0},
+    { firstName: "Mark", lastName: "Holland", gender: 1, salary: 57000, dateOfBirth: new Date("January 15, 1954"),likes: 0, dislikes: 0 },
+    { firstName: "Pam", lastName: "Macintosh", gender: 1, salary: 53000, dateOfBirth: new Date("August 23, 1924"),likes: 0, dislikes: 0 },
+    { firstName: "Todd", lastName: "Barber", gender: 1, salary: 60000, dateOfBirth: new Date("August 12, 1975"), likes: 0, dislikes: 0}, data
 ];
-myApp.controller("myController", function ($scope) {
+app.controller("myController", function ($scope) {
     $scope.message = "Employee Detail";
     $scope.employeeObject = data;
     
